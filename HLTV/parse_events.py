d452007f8401ade4490444c49bd02ee99a962bca
maximzec <maximzec3200@gmail.com>
2021-02-20 19:47:49 +0300

    check TODO

--- HLTV/parse_events.py
+++ HLTV/parse_events.py
@@ -17,17 +17,19 @@ def gen_tab(tab):
 
 url_link = "https://www.hltv.org/events#tab-FEATURED"
 
+# TODO: Придумать как добавить дату в турнир
+
 
 def parse_on_going_events():
     session = requests.session()
     response = session.get(url_link, headers=headers)
-    events = []
+    events = {}
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
         table = soup.find('div', id=gen_tab(tab))
         for event in table.find_all('div', class_='content standard-box'):
-            event_name = event.find('div', class_="text-ellipsis").text
-            events.append(event_name)
+            event_name = event.find('div', class_='text-ellipsis').text
+            events[event_name] = None
         return str(events)
 
     else:
