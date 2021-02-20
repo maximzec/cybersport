12063dcce3e53e91995c1ef18d7bed9be29c904c
maximzec <maximzec3200@gmail.com>
2021-02-20 19:05:40 +0300

    updated

---
+++ HLTV/parse_events.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+from headers import headers
+
+tab = 'All'
+
+
+def gen_tab(tab):
+    tabs = {
+        "Featured": "FEATURED",
+        "Today": "TODAY",
+        "All": "ALL"
+    }
+    assert tab in tabs.keys()
+    return tabs[tab]
+
+
+url_link = "https://www.hltv.org/events#tab-FEATURED"
+
+
+def parse_on_going_events():
+    session = requests.session()
+    response = session.get(url_link, headers=headers)
+    events = []
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        table = soup.find('div', id=gen_tab(tab))
+        for event in table.find_all('div', class_='content standard-box'):
+            event_name = event.find('div', class_="text-ellipsis").text
+            events.append(event_name)
+        return str(events)
+
+    else:
+        return "Not successful status code:" + response.status_code
+
+
+print(parse_on_going_events())
