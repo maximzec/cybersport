b26bf3e91c2fa4e9684d77ec9dbeca71f954ef52
maximzec <maximzec3200@gmail.com>
2021-02-21 00:48:03 +0300

    added constants

---
+++ HLTV/constants/urls.py
@@ -0,0 +1,8 @@
+event_urls = {
+    'events-ongoing-top':}
+
+matches_urls = {
+    'all': 'https://www.hltv.org/matches',
+    'top-tier': 'https://www.hltv.org/matches?predefinedFilter=top_tier',
+    'lan': 'https://www.hltv.org/matches?predefinedFilter=lan_only'
+}
